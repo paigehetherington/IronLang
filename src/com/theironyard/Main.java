803011cb107e3601c91435698e3874cbdb551090
Paige Hetherington <herbmama@gmail.com>
2016-10-16 14:53:34 -0400

    added language functionality

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -4,21 +4,88 @@ package com.theironyard;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.function.BooleanSupplier;
 
 public class Main {
+
+    static HashMap<String, String> combineMaps(ArrayList<HashMap<String, String>> variables) {
+        HashMap<String, String> map = new HashMap<>();
+        for (HashMap<String, String> m : variables) {
+            map.putAll(m);
+        }
+        return map;
+
+    }
+    static ArrayList<String> readLinesInScope(Scanner scanner) {
+
+        ArrayList<String> commands = new ArrayList<>();
+        while (scanner.hasNext()) {
+            String line2 = scanner.nextLine();
+            if (line2.equals("K_STOP")) {
+                break;
+
+            }
+            commands.add(line2);
+        }
+        return commands;
+    }
+
+    public static void processCommand(Scanner scanner, ArrayList<HashMap<String, String>> variables, String line) {
+        line = line.trim(); //trims off whitespace before and after
+        String cmd = line.substring(0, line.indexOf(" "));//get command from line of code
+        String params = line.substring(line.indexOf(" ") +1);
+        HashMap<String, String> vars = combineMaps(variables);
+        if (vars.containsKey(params)) {
+            params = vars.get(params); //pulling from combined map
+        }
+        switch (cmd) {
+            case "PREACH_IT":
+                System.out.println(params);
+                break;
+            case "KEEP_DAT":
+                String variable = params.substring(0, params.indexOf(" "));
+                String value = params.substring(params.indexOf(" ") + 1);
+                variables.get(variables.size() - 1).put(variable, value);
+                break;
+            case "KEEP_DOIN_DAT":
+                int iterations = Integer.valueOf(params);
+                ArrayList<String> commands = readLinesInScope(scanner);
+                for (int i = 0; i < iterations; i++) {
+                    variables.add(new HashMap<>());
+                    for (String command: commands) {
+                        processCommand(scanner, variables, command);
+                    }
+                    variables.remove(variables.size()- 1);
+                }
+                break;
+            case "IF_DAT_BOOTY_KEEPS_POPPIN":
+                boolean condition = Boolean.valueOf(params);
+                ArrayList<String> commands2 = readLinesInScope(scanner);
+                if (condition) {
+                    variables.add(new HashMap<>());
+
+                    for (String command: commands2) {
+                        processCommand(scanner, variables, command);
+                    }
+                    variables.remove(variables.size() - 1);
+                }
+                break;
+        }
+
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
         File f = new File("test.iron");
         Scanner scanner = new Scanner(f);
+        ArrayList<HashMap<String, String>> variables = new ArrayList<>();
+        variables.add(new HashMap<>());
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
-            String cmd = line.substring(0, line.indexOf(" "));//get command from line of code
-            String params = line.substring(line.indexOf(" ") +1);
-            if (cmd.equals("PREACH_IT")) {
-                System.out.println(params);
-            }
+            processCommand(scanner, variables, line); //add scanner as argument instead of making global
         }
 
     }
